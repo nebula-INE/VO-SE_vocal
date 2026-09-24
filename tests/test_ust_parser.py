fb32f8a8aa238a08ca139b461d96b25ddbf74212
nebula-INE <SEKOIA2900@Gmail.com>
2026-09-24 23:25:38 +0900

    Update test_ust_parser.py

--- tests/test_ust_parser.py
+++ tests/test_ust_parser.py
@@ -56,6 +56,37 @@ class TestUstParser(unittest.TestCase):
             self.assertEqual(note.vibrato.cycle, 180.0)   # type: ignore
             self.assertEqual(note.vibrato.depth, 35.0)    # type: ignore
 
+    def test_tempo_prefers_first_note_over_setting(self):
+        """[#SETTING]=120 かつ [#0000] Tempo=170 のとき project.tempo は 170 になる"""
+        ust = (
+            "[#SETTING]\n"
+            "Tempo=120.00\n"
+            "ProjectName=Test\n"
+            "\n"
+            "[#0000]\n"
+            "Length=480\n"
+            "Lyric=か\n"
+            "NoteNum=60\n"
+            "Tempo=170.00\n"
+            "\n"
+            "[#0001]\n"
+            "Length=480\n"
+            "Lyric=か\n"
+            "NoteNum=60\n"
+        )
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            ust_file = os.path.join(tmp_dir, "test.ust")
+            with open(ust_file, "w", encoding="cp932") as f:
+                f.write(ust)
+
+            parser = UstParser()
+            project = parser.load(ust_file)
+
+            self.assertEqual(project.tempo, 170.0)
+            # 各ノートの長さも170BPM換算になっているはず
+            note_dicts = UstConverter.to_note_dicts(project)
+            self.assertAlmostEqual(note_dicts[0]["duration"], 60.0 / 170.0, places=4)
+
     def test_convert_to_note_events(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
             ust_file = os.path.join(tmp_dir, "test.ust")
