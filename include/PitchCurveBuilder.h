6d5709e31d7e75a6bb20b9b086f1aa58018d3cde
nebula-INE <SEKOIA2900@Gmail.com>
2026-09-26 11:38:02 +0900

    fix: apply PBM shapes in native pitch builder

--- include/PitchCurveBuilder.h
+++ include/PitchCurveBuilder.h
@@ -89,6 +89,7 @@ namespace vose_pitch
 
             cpTimes.clear();
             cpValues.clear();
+            cpShapes.clear();
             shapes.clear();
             cpTimes.push_back (pbsOffsetMs);
             cpValues.push_back (pbsStartPitch);
